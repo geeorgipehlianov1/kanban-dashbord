91ef53c1ef1227bb525fad37b2086a0f4945a1a2
Georgi Pehlivanov <george9040@abv.bg>
2024-02-06 16:33:20 +0200

    Implement ecrom section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,17 @@
-const Button = () => {
-  return <div>Button</div>
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
+  return (
+    <button
+      style={{
+        backgroundColor: bgColor,
+        color: color,
+        borderRadius: borderRadius
+      }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      type="button"
+    >
+      {text}
+    </button>
+  )
 }
 
 export default Button
